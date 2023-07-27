c045c8a9f0b972578b2c053936c9f136b6ab791d
Nikolai_Uvarov <nguvarov@gmail.com>
2023-07-27 17:33:53 +0200

    parse input file with regexp

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
 
+	//парсинг аргументов приложения
 	var inpfile, outpfile string
 
 	fmt.Println(os.Args)
@@ -25,6 +28,7 @@ func main() {
 		outpfile = "output.txt"
 	}
 
+	//открытие файла на чтение и создание ридера
 	f, err := os.OpenFile(inpfile, os.O_RDONLY, 0777)
 	if err != nil {
 		panic(err)
@@ -33,6 +37,7 @@ func main() {
 	reader := bufio.NewReader(f)
 	defer f.Close()
 
+	//открытие файла на запись (создание если не существует) и создание райтера
 	o, err := os.OpenFile(outpfile, os.O_WRONLY|os.O_CREATE, os.ModePerm)
 	if err != nil {
 		panic(err)
@@ -40,16 +45,46 @@ func main() {
 	writer := bufio.NewWriter(o)
 	defer o.Close()
 
+	//создание регулярного выражения для поиска арифметических выражений
+	re := regexp.MustCompile("([0-9]+)([+*/-]{1})([0-9]+)")
+
+	//поиск арифметических выражений регулярками и запись результата в буфер
 	for {
 		line, _, err := reader.ReadLine()
 		if err == io.EOF {
 			break
 		}
-		_, err = writer.Write([]byte(string(line) + "\n"))
+
+		sm := re.FindAllStringSubmatch(string(line), 1)
+		if sm == nil {
+			continue
+		}
+
+		fmt.Println(sm[0][0], sm[0][1], sm[0][2], sm[0][3])
+
+		operation := sm[0][0]
+		leftoperand, _ := strconv.Atoi(sm[0][1])
+		operator := sm[0][2]
+		rightoperand, _ := strconv.Atoi(sm[0][3])
+		var result int
+
+		switch operator {
+		case "+":
+			result = leftoperand + rightoperand
+		case "-":
+			result = leftoperand - rightoperand
+		case "*":
+			result = leftoperand * rightoperand
+		case "/":
+			result = leftoperand / rightoperand
+		}
+
+		_, err = writer.Write([]byte(operation + "=" + fmt.Sprint(result) + "\n"))
 		if err != nil {
 			panic(err)
 		}
 	}
+	//запись буфера в файл
 	err = writer.Flush()
 	if err != nil {
 		panic(err)
